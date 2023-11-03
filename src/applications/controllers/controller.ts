08233820679781a868c996af41e63f9a65687276
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-11-03 18:01:29 -0300

    feat: add Controller contract

---
+++ src/applications/controllers/controller.ts
@@ -0,0 +1,13 @@
+import { type HttpResponse, serverError } from '@/applications/helpers'
+
+export abstract class Controller {
+  abstract perform (httpRequest?: any): Promise<HttpResponse>
+
+  async handle (httpRequest?: any): Promise<HttpResponse> {
+    try {
+      return await this.perform(httpRequest)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
